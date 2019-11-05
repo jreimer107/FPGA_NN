3563edc605578cafed9a279931d70b4317ccfe07
John Reimer <jreimer107@gmail.com>
2019-11-05 15:01:33 -0600

    I think decoders were unnecessary

--- Block_Memory.sv
+++ Block_Memory.sv
@@ -1,4 +1,25 @@
-module Block_Memory(iclk, irst_n, iInstAddr, iDataAddr, dataWrite, iData, iNodeAddr, oInstr, oData, oNodes);
+/* Block_Memory.sv
+ * This module is the controller for the block memory. This is the combination
+ * of instruction and data memory. There are three interfaces: instruction
+ * read, data read/write, and node read. Instruction and data read take in a
+ * 16 bit address and output a 16 bit result in the same cycle. Data write does
+ * the same but writing, obviously. Node read instead outputs 16 16-bit values,
+ * each contiguous from the given address.
+ *
+ * Though a 16bit address is input, only 11 bits are actually used.
+ * @input iclk is the clock. Duh.
+ * @input irst_n is an active-low reset.
+ * @input iInstAddr is the address of the instruction to fetch.
+ * @input iDataAddr is the address of the data to retrieve.
+ * @input dataWrite determines if data is being written to memory.
+ * @input iData is the data to be written to memory.
+ * @input iNodeAddr is the base address of the node values to fetch.
+ * @output oInstr is the fetched instruction.
+ * @output oData is the fetched data.
+ * @output oNodes are the 16 fetched nodes.
+*/
+module Block_Memory(iclk, irst_n, iInstAddr, iDataAddr, dataWrite, iData,
+	iNodeAddr, oInstr, oData, oNodes);
 
 input iclk, irst_n;
 input dataWrite;
@@ -9,22 +30,17 @@ output [15:0] oNodes [15:0];
 
 reg [15:0] memory [2048:0];
 
-wire [2048:0] instLoc, dataLoc, nodeLoc;
-decoder Decoder_11_2048(iInstAddr[10:0], instLoc);
-decoder Decoder_11_2048(iDataAddr[10:0], dataLoc);
-decoder Decoder_11_2048(iNodeAddr[10:0], nodeLoc);
-
 always_ff @(posedge clk, negedge rst) begin
 	if (!irst_n)
 		memory <= '{default:0};
 	else begin
 		if (dataWrite)
-			memory[dataLoc] <= iData;
+			memory[iDataAddr] <= iData;
 	end
 end
 
-assign oInstr = memory[instLoc];
-assign oData = memory[dataLoc];
-assign oNodes = memory[nodeLoc+16:nodeLoc];
+assign oInstr = memory[iInstAddr];
+assign oData = memory[iDataAddr];
+assign oNodes = memory[iNodeAddr + 16:iNodeAddr];
 
 endmodule
