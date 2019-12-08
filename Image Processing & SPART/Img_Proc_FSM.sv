088d0d25b88780133e6f77315a6dbffb0d4fdb77
asmita-pal <asmita.pal@wisc.edu>
2019-12-08 15:06:53 -0600

    More work on Img Proc FSM. Still needs a bit more work.  Also removed unused files.

--- Image Processing & SPART/Img_Proc_FSM.sv
+++ Image Processing & SPART/Img_Proc_FSM.sv
@@ -1,15 +1,15 @@
 module Img_Proc_FSM(
 	input clk, pxlclk, rst_n,
 
-	input iCCD_enable, iStart,
+	input iCCD_enable, iCCD_start,
 	input iFVAL,
 	input iDVAL,
 	input [15:0] iDATA,
 
-	output oDmem_wren,
+	output reg oDmem_wren,
 	output reg [6:0] oDmem_addr,
-	output [255:0] oDmem_data,
-	output reg oCCD_Done
+	output reg [15:0] oDmem_data [15:0],
+	output reg oCCD_done
 );
 
 // Buffer FVAL to detect rising edge (new frame)
@@ -34,62 +34,78 @@ always_ff @(posedge clk, negedge rst_n)
 // Pixel count register
 reg [9:0] pxl_cnt;
 wire [3:0] pxl_index = pxl_cnt % 16;
-always_ff (@posedge pxlclk, negedge rst_n) begin
+always_ff @(posedge pxlclk, negedge rst_n) begin
 	if (!rst_n)
 		pxl_cnt <= 0;
 	else begin
 		pxl_cnt <= pxl_cnt;
-		if (state == CAPTURE)
-			if (iDVAL) pxl_cnt <= pxl_cnt + 1;
-		else pxl_cnt <= 0;
+		if (state == CAPTURE) begin
+			if (iDVAL) 
+				pxl_cnt <= pxl_cnt + 1;
+		end
+		else 
+			pxl_cnt <= 0;
 	end
 end
 
 // Dmem write address incrementation and reset
 always_ff @(posedge pxlclk, negedge rst_n)
 	if (!rst_n)
-		dmem_wraddr <= 0;
+		oDmem_addr <= 0;
 	else begin
-		dmem_wraddr <= dmem_wraddr;
-		if ((state == CAPTURE & pxl_cnt != 0 & pxl_index == 0) || 
+		oDmem_addr <= oDmem_addr;
+		if ((state == CAPTURE & pxl_index == 4'hf) || 
 			state == SEND_LAST)
-			dmem_wraddr <= dmem_wraddr + 1;
+			oDmem_addr <= oDmem_addr + 1;
 		else if (state == IDLE)
-			dmem_wraddr <= 0;
+			oDmem_addr <= 0;
 	end
 
 		// if (state == CAPTURE)
 		// 	if (pxl_cnt != 0 & pxl_index == 0) begin
-		// 			dmem_wraddr <= dmem_wraddr + 1;
+		// 			oDmem_addr <= oDmem_addr + 1;
 		// else if (state == SEND_LAST)
-		// 	dmem_wraddr <= dmem_wraddr + 1;
+		// 	oDmem_addr <= oDmem_addr + 1;
 		// else
-		// 	dmem_wraddr <= 0;
+		// 	oDmem_addr <= 0;
 
 
-// oCCD_Done FF
-always_ff (@ posedge clk, negedge rst_n) begin
-	if (!rst_n)
-		oCCD_Done <= 1'b0;
-	else begin
-		oCCD_Done <= oCCD_Done;
-		if (iCCD_enable)
-			if (state == SEND_LAST)
-				oCCD_Done <= 1'b1;
-			else if (state == WAIT)
-				oCCD_Done <= 1'b0;
-		else
-			oCCD_Done <= 1'b0;
+// oCCD_done FF
+// always_ff @(posedge clk, negedge rst_n)
+// 	if (!rst_n)
+// 		oCCD_done <= 1'b0;
+// 	else begin
+// 		oCCD_done <= oCCD_done;
+// 		if (iCCD_enable)
+// 			if (state == SEND_LAST)
+// 				oCCD_done <= 1'b1;
+// 			else if (state == WAIT)
+// 				oCCD_done <= 1'b0;
+// 		else
+// 			oCCD_done <= 1'b0;
+// 	end
+
+reg [15:0] pixels [15:0];
+always_comb begin
+	if (!rst_n) begin
+		oCCD_done = 1'b0;
+		// oDmem_addr = 0;
 	end
 
-
-always_comb begin
+	// oDmem_addr = oDmem_addr;
+	oCCD_done = oCCD_done;
 	next_state = IDLE;
+	oDmem_wren = 1'b0;
+	pixels = pixels;
+
 	case(state)
 		// Wait for button press and enable signal
 		IDLE: begin
-			if (iCCD_enable & iStart)
+			pixels = '{default:0};
+			if (iCCD_enable & iCCD_start) begin
 				next_state = WAIT;
+				oCCD_done = 1'b0;
+			end
 		end
 
 		// Wait for next image to start
@@ -104,21 +120,34 @@ always_comb begin
 		CAPTURE: begin
 			next_state = CAPTURE;
 			if (iDVAL)
-				dmem_wrdata[pxl_index] = iDATA;
-			if (pxl_cnt != 0 & pxl_index == 0)
-				dmem_wren = 1;
+				pixels[pxl_index] = iDATA;
+			if (pxl_index == 4'hf)
+				oDmem_wren = 1;
 			if (pxl_cnt == 783)
 				next_state = SEND_LAST;
 		end
 
 		// 783 not divisible by 16, so send last batch
 		SEND_LAST: begin
-			dmem_wren = 1;
+			oDmem_wren = 1;
 			next_state = IDLE;
+			oCCD_done = 1'b1;
 		end
 
 		default: next_state = IDLE;
 	endcase
 end
+
+
+// Need to do register bypassing on the last pixel so it gets there in time
+always_comb begin
+	oDmem_data = pixels;
+	oDmem_data[pxl_index] = iDATA;
+end
+
+// assign oDmem_data = {iDATA, pixels[14:0]};
+// assign oDmem_data = pxl_index == 4'h0 ? {pixels[15:1], iDATA} :
+// 					pxl_index == 4'hf ? {iDATA, pixels[14:0]} :
+// 					{pixels[15 : pxl_index + 1], iDATA, pixels[pxl_index - 1 : 0]};
 
 endmodule
---
+++ Image Processing & SPART/TB/Img_Proc_FSM_tb.sv
@@ -0,0 +1,95 @@
+module Img_Proc_FSM_tb();
+
+reg clk, pxlclk, rst_n;
+
+reg enable, start;
+reg FVAL, DVAL;
+reg [15:0] DATA;
+
+wire dmem_wren;
+wire [6:0] dmem_addr;
+wire [15:0] dmem_data [15:0];
+wire done;
+
+Img_Proc_FSM DUT(
+    .clk(clk),
+    .pxlclk(pxlclk),
+    .rst_n(rst_n),
+
+    .iCCD_enable(enable),
+    .iCCD_start(start),
+
+    .iFVAL(FVAL),
+    .iDVAL(DVAL),
+    .iDATA(DATA),
+
+    .oDmem_wren(dmem_wren),
+    .oDmem_addr(dmem_addr),
+    .oDmem_data(dmem_data),
+    
+    .oCCD_done(done)
+);
+
+always #5 clk = ~clk;
+
+integer pxl_cnt;
+integer clk_cnt;
+always_ff @(posedge clk, negedge rst_n) begin
+    if (!rst_n)
+        clk_cnt = 0;
+    else
+        clk_cnt <= clk_cnt + 1;
+end
+
+assign DATA = pxl_cnt;
+assign DVAL = FVAL;
+assign pxlclk = clk;
+
+
+initial begin
+    clk = 0;
+    rst_n = 0;
+    enable = 0;
+    start = 0;
+    FVAL = 0;
+
+
+    repeat(2) @ (negedge clk);
+    rst_n = 1;
+    repeat(3) @ (posedge clk);
+    enable = 1;
+    repeat(3) @ (posedge clk);
+    start = 1;
+    repeat (3) @ (posedge clk);
+    start = 0;
+
+    repeat(3) @ (posedge clk);
+
+    FVAL = 1;
+    repeat (2) @ (posedge clk);
+    
+    pxl_cnt = 0;
+    $display("Starting pixels at clock count %d", clk_cnt);
+
+    repeat(784) @ (posedge clk) pxl_cnt = pxl_cnt + 1;
+    FVAL = 0;
+    $display("Stopping pixels at clock count %d", clk_cnt);
+
+
+end
+
+integer wren_cnt;
+always_ff @(posedge clk, negedge rst_n)
+    if (~rst_n)
+        wren_cnt = 0;
+    else if (dmem_wren) begin
+        wren_cnt = wren_cnt + 1;
+        $display("Written to %h: %p", dmem_addr, dmem_data);
+    end
+    
+
+initial begin
+    #50000 $stop;
+end
+
+endmodule
